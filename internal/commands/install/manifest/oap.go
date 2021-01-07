f509bd18a146a859104e0ea7e5b219e0b6de3fa6
Gao Hongtao <hanahmily@gmail.com>
2021-01-07 16:24:17 +0800

    Add install command and manifest sub command (#84)
    * Add install command and manifest sub command
    
    Signed-off-by: Gao Hongtao <hanahmily@gmail.com>
    
    * Update mod
    
    Signed-off-by: Gao Hongtao <hanahmily@gmail.com>
    
    * Polish go.sum
    
    Signed-off-by: Gao Hongtao <hanahmily@gmail.com>
    
    * Increase lint timeout
    
    Signed-off-by: Gao Hongtao <hanahmily@gmail.com>
    
    * Upgrade swck
    
    Signed-off-by: Gao Hongtao <hanahmily@gmail.com>
    
    * Update internal/commands/install/manifest/manifest.go
    
    Co-authored-by: Zhenxu Ke <kezhenxu94@apache.org>
    
    * Fix default template
    
    Signed-off-by: Gao Hongtao <hanahmily@gmail.com>
    
    * Update assets
    
    Signed-off-by: Gao Hongtao <hanahmily@gmail.com>
    
    Co-authored-by: Zhenxu Ke <kezhenxu94@apache.org>

---
+++ internal/commands/install/manifest/oap.go
@@ -0,0 +1,60 @@
+// Licensed to Apache Software Foundation (ASF) under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Apache Software Foundation (ASF) licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package manifest
+
+import (
+	"fmt"
+
+	operatorv1alpha1 "github.com/apache/skywalking-swck/apis/operator/v1alpha1"
+	"github.com/urfave/cli"
+	"gopkg.in/yaml.v2"
+	controllerruntime "sigs.k8s.io/controller-runtime"
+
+	"github.com/apache/skywalking-cli/assets"
+)
+
+var oapCmd = cli.Command{
+	Name:      "oap",
+	ShortName: "o",
+	Usage:     "Output the Kubernetes manifest for installing OAP server to stdout",
+	UsageText: usage("oap"),
+	Flags:     flags,
+	Action: func(ctx *cli.Context) error {
+		base := &operatorv1alpha1.OAPServer{
+			TypeMeta: controllerruntime.TypeMeta{
+				Kind: "OAPServer",
+			},
+			ObjectMeta: controllerruntime.ObjectMeta{
+				Name:      ctx.String("name"),
+				Namespace: ctx.String("namespace"),
+			},
+		}
+		err := yaml.Unmarshal([]byte(assets.Read("cr/oap.yaml")), base)
+		if err != nil {
+			return fmt.Errorf("failed to convert yaml to OAPServer: %v", err)
+		}
+		base.Default()
+		if err := base.ValidateCreate(); err != nil {
+			return fmt.Errorf("failed to validate OAPServer: %v", err)
+		}
+		if err := render("oapserver", ctx, base, &operatorv1alpha1.OAPServer{}); err != nil {
+			return err
+		}
+		return nil
+	},
+}
