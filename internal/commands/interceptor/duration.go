6503ed83bc099319dc60100313cf8f33057ee35a
kezhenxu94 <kezhenxu94@apache.org>
2021-10-26 19:09:56 +0800

    Allow setting `start` `end` with relative time

--- internal/commands/interceptor/duration.go
+++ internal/commands/interceptor/duration.go
@@ -18,6 +18,7 @@
 package interceptor
 
 import (
+	"fmt"
 	"strconv"
 	"time"
 
@@ -28,9 +29,10 @@ import (
 	"github.com/urfave/cli/v2"
 
 	"github.com/apache/skywalking-cli/internal/logger"
+	"github.com/apache/skywalking-cli/internal/model"
 )
 
-func TryParseTime(unparsed string) (api.Step, time.Time, error) {
+func TryParseTime(unparsed string, userStep api.Step) (api.Step, time.Time, error) {
 	var possibleError error
 	for step, layout := range utils.StepFormats {
 		t, err := time.Parse(layout, unparsed)
@@ -39,7 +41,11 @@ func TryParseTime(unparsed string) (api.Step, time.Time, error) {
 		}
 		possibleError = err
 	}
-	return api.StepSecond, time.Time{}, possibleError
+	duration, err := time.ParseDuration(unparsed)
+	if err == nil {
+		return userStep, time.Now().Add(duration), nil
+	}
+	return userStep, time.Time{}, fmt.Errorf("the given time %v is neither absolute time nor relative time: %+v %+v", unparsed, possibleError, err)
 }
 
 // DurationInterceptor sets the duration if absent, and formats it accordingly,
@@ -47,9 +53,20 @@ func TryParseTime(unparsed string) (api.Step, time.Time, error) {
 func DurationInterceptor(ctx *cli.Context) error {
 	start := ctx.String("start")
 	end := ctx.String("end")
-	timezone := ctx.String("timezone")
+	userStep := ctx.Generic("step")
+	if timezone := ctx.String("timezone"); timezone != "" {
+		if offset, err := strconv.Atoi(timezone); err == nil {
+			// `offset` is in form of "+1300", while `time.FixedZone` takes offset in seconds
+			time.Local = time.FixedZone("", offset/100*60*60)
+		}
+	}
 
-	startTime, endTime, step, dt := ParseDuration(start, end, timezone)
+	var s api.Step
+	if userStep != nil {
+		s = userStep.(*model.StepEnumValue).Selected
+	}
+
+	startTime, endTime, step, dt := ParseDuration(start, end, s)
 
 	if err := ctx.Set("start", startTime.Format(utils.StepFormats[step])); err != nil {
 		return err
@@ -71,19 +88,10 @@ func DurationInterceptor(ctx *cli.Context) error {
 //   then: end := now + 30 units, where unit is the precision of `start`, (hours, minutes, etc.)
 // if --start is absent, --end is given,
 //   then: start := end - 30 units, where unit is the precision of `end`, (hours, minutes, etc.)
-func ParseDuration(start, end, timezone string) (startTime, endTime time.Time, step api.Step, dt utils.DurationType) {
-	logger.Log.Debugln("Start time:", start, "end time:", end, "timezone:", timezone)
+func ParseDuration(start, end string, userStep api.Step) (startTime, endTime time.Time, step api.Step, dt utils.DurationType) {
+	logger.Log.Debugln("Start time:", start, "end time:", end, "timezone:", time.Local)
 
 	now := time.Now()
-
-	if timezone != "" {
-		if offset, err := strconv.Atoi(timezone); err == nil {
-			// `offset` is in form of "+1300", while `time.FixedZone` takes offset in seconds
-			now = now.In(time.FixedZone("", offset/100*60*60))
-
-			logger.Log.Debugln("Now:", now, "with server timezone:", timezone)
-		}
-	}
 
 	// both are absent
 	if start == "" && end == "" {
@@ -94,23 +102,21 @@ func ParseDuration(start, end, timezone string) (startTime, endTime time.Time, s
 
 	// both are present
 	if len(start) > 0 && len(end) > 0 {
-		start, end = AlignPrecision(start, end)
-
-		if _, startTime, err = TryParseTime(start); err != nil {
+		if userStep, startTime, err = TryParseTime(start, userStep); err != nil {
 			logger.Log.Fatalln("Unsupported time format:", start, err)
 		}
-		if step, endTime, err = TryParseTime(end); err != nil {
+		if step, endTime, err = TryParseTime(end, userStep); err != nil {
 			logger.Log.Fatalln("Unsupported time format:", end, err)
 		}
 
 		return startTime, endTime, step, utils.BothPresent
 	} else if end == "" { // end is absent
-		if step, startTime, err = TryParseTime(start); err != nil {
+		if step, startTime, err = TryParseTime(start, userStep); err != nil {
 			logger.Log.Fatalln("Unsupported time format:", start, err)
 		}
 		return startTime, startTime.Add(30 * utils.StepDuration[step]), step, utils.EndAbsent
 	} else { // start is absent
-		if step, endTime, err = TryParseTime(end); err != nil {
+		if step, endTime, err = TryParseTime(end, userStep); err != nil {
 			logger.Log.Fatalln("Unsupported time format:", end, err)
 		}
 		return endTime.Add(-30 * utils.StepDuration[step]), endTime, step, utils.StartAbsent
