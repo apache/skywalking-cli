5f9bdc04b60b1aed7f88286cdb7e629b0cad3f2a
zhengziyi <b21080426@njupt.edu.cn>
2024-10-26 16:18:43 +0800

    fix: golint

--- internal/commands/interceptor/instance.go
+++ internal/commands/interceptor/instance.go
@@ -149,7 +149,7 @@ func parseInstanceSlice(required bool, idSliceFlagName, nameSliceFlagName, servi
 	}
 }
 
-func encode(serviceID, nameFlagName, id, name string) (encodedId, encodedName string, err error) {
+func encode(serviceID, nameFlagName, id, name string) (encodedID, encodedName string, err error) {
 	if id != "" {
 		parts := strings.Split(id, "_")
 		if len(parts) != 2 {
